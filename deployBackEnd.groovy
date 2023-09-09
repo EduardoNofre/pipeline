f0f4caf089eafa4c6eb50377d63bca379bb1bc3c
Eduardo Sa <nofrereis@gmail.com>
2023-09-09 02:00:46 -0300

    Update deployBackEnd.groovy
    ${userNameServer}@${server}

--- deployBackEnd.groovy
+++ deployBackEnd.groovy
@@ -215,7 +215,7 @@ def metodoDeployServer() {
     echo " -------------- STOP SERVICE DIGITAL-CONFIG-SERVICE -------------- "
     echo " ----------------------------------------------------------------- "
 
-    sh "sudo systemctl stop digital-config-service.service"
+    sh " ${userNameServer}@${server} sudo systemctl stop digital-config-service.service"
     //sh "ssh -tt -o StrictHostKeyChecking=no ${userNameServer}@${server} sudo systemctl stop digital-config-service.service"
     sh "sleep 5"
 
