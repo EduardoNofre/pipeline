70fb1542280e7028e4248e504dc8963c8444c727
eduardo sa <nofrereis@gmail.com>
2023-12-29 20:39:30 -0300

    correção da credencial no script o nome

--- deployContentWeb.groovy
+++ deployContentWeb.groovy
@@ -49,7 +49,7 @@ def deployApp(gitUrl) {
         // TOKEN CRIADO NO GIT E ASSOCIADO AO JNEKINS
         // DEVE SER CRIAR UMA CREDENCIAL USANDO USUARIO E SENHA  DO GITHUB :NAME 'github-jenkin'
         // ---------------------------------------------------------------------------------------------- 
-        credentialsId: "github-jenkins"
+        credentialsId: "Jenkins-acess-gitHub"
 
       	// NOME PROPERTIES DA APLICAÇAO.
       	def nomeProperties = "application.properties"
